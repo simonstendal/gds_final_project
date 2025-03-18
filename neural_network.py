6e0b4b5abf041a1ba57f0ed0537e242f14d9c34d
simonstendal <simonstendal@github.com>
2025-03-18 21:28:22 +0100

    changes to neural network - 88% F1

--- neural_network.py
+++ neural_network.py
@@ -4,7 +4,7 @@ import wandb
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, classification_report
 from logistic_regression import label_entry
 import wandb.integration
 
@@ -15,9 +15,9 @@ def neural_network(train_x:pd.Series, train_y:pd.Series, labels):
     """
     Initialize an SKLearn classifier neural network.
     """
-    classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    classifier = MLPClassifier(solver='adam', alpha=1e-4, hidden_layer_sizes=(128, 64, 32), activation='relu', random_state=1, max_iter=500)
 
-    vector_col = vectorizer.fit_transform(train_x).astype(np.float32)
+    vector_col = vectorizer.fit_transform(train_x)
     label_col = train_y.to_numpy()
     print("Done vectorizing data.")
     classifier.fit(vector_col, label_col)
@@ -26,23 +26,31 @@ def neural_network(train_x:pd.Series, train_y:pd.Series, labels):
 
 def test_model(model:MLPClassifier, val_x:pd.Series, val_y:pd.Series, test_X=None, test_Y=None, wandb_init = False):
     label_col = val_y.to_numpy()
-    vector_col = vectorizer.transform(val_x).astype(np.float32)
+    vector_col = vectorizer.transform(val_x)
 
     # Evaluate scores:
-    val_score = model.score(vector_col, label_col)
+    val_pred = model.predict(vector_col)
+    val_f1 = f1_score(label_col, val_pred, average='weighted')
+    class_report = classification_report(label_col, val_pred)
+    print("CLASS REPORT VALIDATION DATA:")
+    print(class_report)
 
     if wandb_init:
         wandb.init(project="gds-project-test")
-        wandb.log({ "val_accuracy": val_score,})
+        wandb.log({"val_f1": val_f1})
         wandb.finish()
 
     if test_X and test_Y:
-        vector_col_test = vectorizer.transform(test_X).astype(np.float32)
+        vector_col_test = vectorizer.transform(test_X)
         label_col_test = test_Y.to_numpy()
-        test_score = model.score(vector_col_test, label_col_test)
-        return val_score, test_score
+        test_pred = model.predict(vector_col_test)
+        test_f1 = f1_score(label_col_test, test_pred, average='weighted')
+        class_report_test = classification_report(label_col, val_pred)
+        print("CLASS REPORT TEST DATA:")
+        print(class_report_test)
+        return test_f1
     else:
-        return val_score
+        return val_f1
 
 if __name__ == "__main__":
     """
@@ -53,7 +61,7 @@ if __name__ == "__main__":
     unwanted_labels = ["unreliable", "rumor", "unknown", "2018-02-10 13:43:39.521661"]
 
     #split corpus data
-    data = pd.read_csv("cleaned_corpus_typed.csv").dropna()
+    data = pd.read_csv("cleaned_corpus.csv").dropna()
     data.drop(data.index[(data["type"].isin(unwanted_labels))],axis=0,inplace=True)
     data_X = data['content']
     data_Y = data['type'].apply(label_entry, args=(labels,))
@@ -67,5 +75,5 @@ if __name__ == "__main__":
     print("Done training model.")
 
     test = test_model(model, X_val, Y_val)
-    print(f"VAL_SCORE: {test}")
+    print(f"VALIDATION DATA F1: {test}")
 
