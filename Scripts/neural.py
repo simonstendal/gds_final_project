5552aeaa4bf6c57993d295c3c0d3f3f19d975a83
Sebastian <sebastianmcconnell01@gmail.com>
2025-03-24 00:20:21 +0100

    f1 score

--- Scripts/neural.py
+++ Scripts/neural.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import StratifiedKFold, cross_val_score, train_test
 from collections import Counter
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import f1_score
+from sklearn.neural_network import MLPClassifier
 import pandas as pd
 import numpy as np
 import wandb
@@ -22,33 +23,35 @@ def label_entry(type, labels):
     Label one document as fake or reliable, defaults to None (which can be removed with .dropna()).
     """
     if type in labels[0]:
-        return 0
+        return "fake"
     elif type in labels[1]:
-        return 1
+        return "reliable"
     else:
         print(f"MISSED TYPE:{type}")
         return None
+
 
 def logistic_regression(train_x:pd.Series, train_y:pd.Series, labels):
     """
     Initialize an SKLearn Logistic regression model.
     """
     vector_col = vectorizer.fit_transform(train_x).astype(np.float32)
-    label_col = train_y
+    label_col = train_y.to_numpy()
     print("Done vectorizing data.")
-    model = LogisticRegression(class_weight="balanced", random_state=16, solver="saga", max_iter=1000, n_jobs=-1)
+    model = MLPClassifier(random_state=1, max_iter=300).fit(vector_col, label_col)
     print("Done iterating data.")
-    model.fit(vector_col, label_col)
+    
     print("Done training model.")
     return model
 
 def test_model(model:LogisticRegression, val_x:pd.Series, val_y:pd.Series, test_X=None, test_Y=None, wandb_init = False):
-    label_col = val_y
+    label_col = val_y.to_numpy()
     vector_col = vectorizer.transform(val_x).astype(np.float32)
 
     # Evaluate scores:
-    val_predict = model.predict(vector_col)
-    val_score = f1_score(label_col, val_predict)
+    model.predict_proba(vector_col[:1])
+    model.predict(vector_col[:5, :])
+    val_score = model.score(vector_col, label_col)
 
     if wandb_init:
         wandb.init(project="gds-project-test")
@@ -57,9 +60,10 @@ def test_model(model:LogisticRegression, val_x:pd.Series, val_y:pd.Series, test_
 
     if test_X is not None and test_Y is not None:
         vector_col_test = vectorizer.transform(test_X).astype(np.float32)
-        label_col_test = test_Y
-        test_predict = model.predict(vector_col_test)
-        test_score = f1_score(label_col_test, test_predict)
+        label_col_test = test_Y.to_numpy()
+        model.predict_proba(vector_col_test[:1])
+        model.predict(vector_col_test[:5, :])
+        test_score = model.score(vector_col_test, label_col_test)
         return val_score, test_score
     else:
         return val_score
@@ -73,36 +77,20 @@ if __name__ == "__main__":
     unwanted_labels = ["unreliable", "rumor", "unknown", "2018-02-10 13:43:39.521661"]
 
     #split corpus data
-    train = pd.read_csv("Data_in_csv/train.csv").dropna()
-    train.drop(train.index[(train["type"].isin(unwanted_labels))],axis=0,inplace=True)
-    train_X = train['content']
-    train_Y = train['type'].apply(label_entry, args=(labels,)).to_numpy().astype(np.float32)
-
-    validation = pd.read_csv("Data_in_csv/validation.csv").dropna()
-    validation.drop(validation.index[(validation["type"].isin(unwanted_labels))],axis=0,inplace=True)
-    validation_X = validation['content']
-    validation_Y = validation['type'].apply(label_entry, args=(labels,)).to_numpy().astype(np.float32)
-
-    test = pd.read_csv("Data_in_csv/test.csv").dropna()
-    test.drop(test.index[(test["type"].isin(unwanted_labels))],axis=0,inplace=True)
-    test_X = test['content']
-    test_Y = test['type'].apply(label_entry, args=(labels,)).to_numpy().astype(np.float32)
-
-    bbc_data = pd.read_csv("Data_in_csv/bbc_processed.csv").dropna()
-    bbc_data_X = bbc_data['content']
-    bbc_data_Y = bbc_data['type'].apply(label_entry, args=(labels,)).to_numpy().astype(np.float32)
-
-    X_val_bbc = pd.concat([validation_X, bbc_data_X])
-    Y_val_bbc = np.concat([validation_Y, bbc_data_Y])
+    data = pd.read_csv("../news_sample_processed.csv").dropna()
+    data.drop(data.index[(data["type"].isin(unwanted_labels))],axis=0,inplace=True)
+    data_X = data['content']
+    data_Y = data['type'].apply(label_entry, args=(labels,))
     print("Done getting data ready.")
 
-    model = logistic_regression(train_X, train_Y, labels)
+    X_train, X_val_test, Y_train, Y_val_test = train_test_split(data_X, data_Y, test_size=0.2, random_state=16)
+    X_val, X_test, Y_val, Y_test = train_test_split(X_val_test, Y_val_test, test_size=0.5, random_state=16)
+    print("Done splitting data.")
+
+    model = logistic_regression(X_train, Y_train, labels)
     print("Done training model.")
-    val_score = test_model(model, validation_X, validation_Y)
-    bbc_val_score = test_model(model, X_val_bbc, Y_val_bbc)
-    test_score = test_model(model, test_X, test_Y)
 
-    print(f"f1 validation score without bbc data: {val_score}")
-    print(f"f1 validation score with bbc data: {bbc_val_score}")
-    print(f"f1 test score: {test_score}")
+    val_score, test_score = test_model(model, X_val, Y_val, X_test, Y_test)
 
+    print(f"Validation score: {val_score}")
+    print(f"Test score: {test_score}")
